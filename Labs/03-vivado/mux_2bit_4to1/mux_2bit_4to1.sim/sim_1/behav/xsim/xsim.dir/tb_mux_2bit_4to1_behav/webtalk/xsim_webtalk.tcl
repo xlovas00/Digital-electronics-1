46d4344c2d6864423dc7ce1447de93a829afb0fd
xlovas00 <xlovas00@stud.feec.vutbr.cz>
2021-02-24 17:57:26 +0100

    modified Images

--- Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/xsim_webtalk.tcl
+++ Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Feb 24 16:59:04 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Feb 24 17:56:58 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "3e77bf8f-c6f2-4cf2-84b8-12c031706d37" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "6d39ff1d112b4ef79d206e69b749d167" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "16" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "17" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3600 6-Core Processor              " -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6488_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3032313881 -regid "" -xml C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6480_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3371002885 -regid "" -xml C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.sim/sim_1/behav/xsim/xsim.dir/tb_mux_2bit_4to1_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
