48eaeb46243aff1b28dd84b2530ee7e72624f855
xlovas00 <xlovas00@stud.feec.vutbr.cz>
2021-03-17 20:07:07 +0100

    added timewaveform

--- Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/xsim_webtalk.tcl
+++ Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Mar 17 16:03:03 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Mar 17 20:05:13 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "3e77bf8f-c6f2-4cf2-84b8-12c031706d37" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "2cbe4492f57849c5b3d6dd4cdb5b4958" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "11" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3600 6-Core Processor              " -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6548_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2080156079 -regid "" -xml C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6484_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 912129530 -regid "" -xml C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/vacla/Documents/xlovas00/Digital-electronics-1/Labs/06-display_driver/display_driver/display_driver.sim/sim_1/behav/xsim/xsim.dir/tb_driver_7seg_4digits_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
